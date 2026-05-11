e81567097ada8b662920b31b4aff2e6b2ee43491
joacopignotti <joacopignotti1@gmail.com>
2026-05-11 18:50:41 -0300

    Agrego perfilController, perfilRoutes y modifico server.js para agregar la ruta de perfil.

---
+++ controllers/perfilController.js
@@ -0,0 +1,34 @@
+  try {
+    const data = await fs.readFile('./data/perfil.json', 'utf-8');
+    const perfil = JSON.parse(data);
+    return res.status(200).json(perfil);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ error: 'No se pudo obtener el perfil' });
+  }
+
+const getPerfilById = async (req, res) => {
+  try {
+    const data = await fs.readFile('./data/perfilDetalle.json', 'utf8');
+    const perfiles = JSON.parse(data);
+    const { id } = req.params;
+    const perfilId = perfiles.find((p) => p.id === parseInt(id));
+    if (!perfilId) {
+      return res
+        .status(404)
+        .json({ error: `No existe el perfil con id ${id}` });
+    }
+    return res.status(200).json(perfilId);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ error: `No se pudo obtener el detalle del perfil con id ${id}` });
+  }
+};
+
+module.exports = { getPerfil, getPerfilById };
+
+
