6bf1578b65ed55c353cf1b4dadd18e5b1628d5c7
FrancoSini <francosinigaglia2003@gmail.com>
2026-05-11 22:23:45 -0300

    11-5 commit con perfil

--- models/server.js
+++ models/server.js
@@ -17,7 +17,7 @@ class Server {
   rutas() {
     this.app.use('/servicios', require('../routes/serviciosRoutes'));
     this.app.use('/equipo', require('../routes/equipoRoutes'));
-    this.app.use('/perfil', require('../routes/perfilRoutes'));
+    //this.app.use('/perfil', require('../routes/perfilRoutes'));
     this.app.use('/login', require('../routes/loginRoutes'));
 
     // manejo de errores
